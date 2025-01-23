cbc6b1e136be04f206d6db0f3ba154e8f428969b
SamF-creator <biomed2020@gmail.com>
2025-01-23 11:04:07 +0300

    5th version

--- littlelemon/reservation/urls.py
+++ littlelemon/reservation/urls.py
@@ -6,14 +6,14 @@ from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 from .views import MenuViewSet, BookingViewSet
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import MenuViewSet, BookingViewSet, UserViewSet
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', UserViewSet)
 router.register(r'menu', MenuViewSet)
 router.register(r'booking', BookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('msg/', views.msg, name='msg'),
     path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/reservation/views.py
+++ littlelemon/reservation/views.py
@@ -1,36 +1,35 @@
-#views.py
-from django.http import HttpResponse
-from rest_framework import permissions
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from .models import Menu, Booking, User
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-
-
-# Create your views here. 
-
+# Protected view
 @api_view()
 @permission_classes([IsAuthenticated])
 def msg(request):
-    return Response({"message":"This view is protected"})
+    return Response({"message": "This view is protected"})
+
+# ViewSet for Menu to handle single item retrieval
 class MenuViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.all()
+    queryset = Menu.objects.all()  # All menu items in the database
     serializer_class = MenuSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Override the default retrieve method to handle getting a single menu item.
+        This is automatically handled by `ModelViewSet`, but you can override if needed.
+        """
+        return super().retrieve(request, *args, **kwargs)
 
-@api_view()
-@permission_classes([IsAuthenticated])
-def msg(request):
-    return Response({"message":"This view is protected"})
+# ViewSet for Booking
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+# ViewSet for User
 class UserViewSet(viewsets.ModelViewSet):
-   queryset = User.objects.all()
-   serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
